49de1176b140ccce6d8219573ff9eb52abdc956d
=lsh <3028429906@qq.com>
2022-09-21 19:50:47 +0800

    redis缓存菜品

--- reggie_take_out/src/main/java/com/itheima/reggie/controller/DishController.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/controller/DishController.java
@@ -16,10 +16,12 @@ import com.itheima.reggie.servive.DishService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @RestController
@@ -32,6 +34,8 @@ public class DishController {
     private DishFlavorService dishFlavorService;
     @Autowired
     private CategoryService categoryService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 新增菜品
@@ -133,7 +137,19 @@ public class DishController {
      * @return
      */
     @GetMapping("/list")
-    public R<List<Dish>> list(Dish dish) {
+    public R<List<DishDto>> list(Dish dish) {
+        List<DishDto> listDto=null;
+        //动态构造key  每个分类都不同 ，所以每次查询的key不同
+        String key = "dish_"+dish.getCategoryId()+"_"+dish.getStatus();  //dish_1397844263642378242_1
+        //先从redis中获取数据
+        listDto = (List<DishDto>) redisTemplate.opsForValue().get(key);
+
+        if (listDto != null) {
+            //存在数据，直接返回
+            return R.success(listDto);
+
+        }
+        //不存在，查询数据库，将数据缓存到redis
         LambdaQueryWrapper<Dish> lambdaQueryWrapper = new LambdaQueryWrapper();
         lambdaQueryWrapper.eq(dish.getCategoryId() != null, Dish::getCategoryId, dish.getCategoryId());
         //添加条件，只查询在售菜品
@@ -142,27 +158,30 @@ public class DishController {
         lambdaQueryWrapper.orderByAsc(Dish::getSort).orderByDesc(Dish::getUpdateTime);
         //查询出在售菜品且根据菜品的分类查出来的菜品
         List<Dish> list = dishService.list(lambdaQueryWrapper);
-//        List<DishDto> listDto = list.stream().map((item) -> {
-//            DishDto dishDto = new DishDto();
-//            BeanUtils.copyProperties(item, dishDto);
-//            Long categoryId = item.getCategoryId();
-//            //根据id分类查询对象
-//            Category category = categoryService.getById(categoryId);
-//            if (category != null) {
-//                String categoryName = category.getName();
-//                dishDto.setCategoryName(categoryName);
-//            }
-//            //当前菜品id
-//            Long dishId = item.getId();
-//            LambdaQueryWrapper<DishFlavor> FlavorLqw = new LambdaQueryWrapper<DishFlavor>()
-//                                                        .eq(DishFlavor::getDishId, dishId);
-//            //根据菜品id查找对应的口味
-//            List<DishFlavor> dishFlavors = dishFlavorService.list(FlavorLqw);
-//            //将口味信息设置到里面
-//            dishDto.setFlavors(dishFlavors);
-//            return dishDto;
-//        }).collect(Collectors.toList());
-        return R.success(list);
+        //在主页面展示菜品的口味数据
+         listDto = list.stream().map((item) -> {
+            DishDto dishDto = new DishDto();
+            BeanUtils.copyProperties(item, dishDto);
+            Long categoryId = item.getCategoryId();
+            //根据id分类查询对象
+            Category category = categoryService.getById(categoryId);
+            if (category != null) {
+                String categoryName = category.getName();
+                dishDto.setCategoryName(categoryName);
+            }
+            //当前菜品id
+            Long dishId = item.getId();
+            LambdaQueryWrapper<DishFlavor> FlavorLqw = new LambdaQueryWrapper<DishFlavor>()
+                                                        .eq(DishFlavor::getDishId, dishId);
+            //根据菜品id查找对应的口味
+            List<DishFlavor> dishFlavors = dishFlavorService.list(FlavorLqw);
+            //将口味信息设置到里面
+            dishDto.setFlavors(dishFlavors);
+            return dishDto;
+        }).collect(Collectors.toList());
+         //缓存到redis
+        redisTemplate.opsForValue().set(key,listDto,60, TimeUnit.MINUTES);
+        return R.success(listDto);
     }
 
     /**
